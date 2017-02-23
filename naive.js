b4ec55add2c87279003aca86c0b5e87a3c0a0f41
Louis Capitanchik <contact@louiscap.co>
2017-02-23 21:23:59 +0000

    OASIDN

--- naive.js
+++ naive.js
@@ -1,28 +1,64 @@
 const getData = require('./parse')
+const fs = require('fs-jetpack')
 
 async function exec() {
 	const [originalEndpoints, headers, sizes] = await getData()
 	const caches = {}
-	const initCache = () => ({
-		usage: 0,
-		size: headers.fileSize,
-		videos: [],
-	})
+
+	const getCache = id => {
+		if (caches[id] != null) {
+			return caches[id]
+		} else {
+			const newCache = {
+				id,
+				capacity: headers.fileSize,
+				videos: [],
+			}
+			console.log(newCache)
+			caches[id] = newCache
+			return newCache
+		}
+	}
+
 	const getMostRequested = (videos) => {
 		let res = { id: null, requests: 0 }
-		Object.entries(([id, reqs]) => {
-			if (res.requests > reqs) {
-				res = { id, requests: reqs}
+		Object.entries(videos).forEach(([ id, reqs ]) => {
+			if (res.requests < reqs) {
+				res = { id, requests: reqs }
 			}
 		})
-		return videos
+		return res
+	}
+
+	const getClosestCache = (size, datacentre, caches) => {
+		let res = { id: null, distance: datacentre, c :null }
+		Object.entries(caches).forEach(([id, d]) => {
+			if (d.ping < res.distance) {
+				const cache = getCache(id)
+				if (cache.capacity > size) {
+					res = { id, distance: d.ping, c: cache }
+				}
+			}
+		})
+		return res
 	}
 
 	const endpoints = Object.assign({}, originalEndpoints)
 
 	Object.entries(endpoints).forEach(([endpointId, endpointData]) => {
-
+		const mostRequested = getMostRequested(endpointData.requests)
+		if (mostRequested.id == null) return
+		console.log(mostRequested)
+		delete endpointData[mostRequested.id]
+		const cache = getClosestCache(mostRequested.requests, endpointData.datacenter, endpointData.connections)
+		console.log(cache)
+		if (cache.id != null) {
+			cache.c.videos.push(mostRequested)
+			cache.c.capacity -= mostRequested.requests
+		}
 	})
+
+	fs.write('wut.json', caches)
 }
 
 exec()
