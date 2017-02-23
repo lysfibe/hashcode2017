f539347bff1c35c24f4c5ad904db5276dba01ab9
Louis Capitanchik <contact@louiscap.co>
2017-02-23 19:37:51 +0000

    It was actually wrong, soz guys 🎅

--- parse.js
+++ parse.js
@@ -6,17 +6,24 @@ async function exec() {
 	const [headers, sizes, ...rows] = data.split('\n')
 	const [noVideos, noEndpoints, noDescriptors, noServers, fileSize] = headers.split(' ')
 
+	const sizeList = sizes.split(' ')
+
 	console.log(noVideos, 'videos')
 	console.log(noEndpoints, 'endpoints')
 	console.log(noDescriptors, 'request descriptors')
 	console.log(noServers, 'cache servers')
 	console.log(fileSize, 'server size')
 
-	let id = {}
+	let endpoints = {}
+	let videoData = {}
 
 	let cur = 0
 	let total = 0
 	let focus = 0
+
+	let endpoint = -1
+
+	let videoCount = 0
 
 	let parsingRowData = false
 
@@ -27,24 +34,26 @@ async function exec() {
 		if (z == null) {
 			// Parsing block the middle block
 			if (parsingRowData) {
-				id[focus].push({ to: Number(x), ping: Number(y) })
+				endpoints[focus].connections.push({ to: Number(x), ping: Number(y) })
 				cur += 1
 				if (total === cur) {
 					parsingRowData = false
 				}
 			} else {
-				id[Number(x)] = []
-				focus = Number(x)
+				endpoint += 1
+				endpoints[endpoint] = { datacenter: Number(x), connections: [], requests: {} }
+				focus = endpoint
 				total = Number(y)
 				cur = 0
 				parsingRowData = true
 			}
 		} else {
 			// Parsing request
+			endpoints[y].requests[x] = z
 		}
 	})
 
-	fs.write('./' + file + '.json', id)
+	fs.write('./' + file + '.json', endpoints)
 
 }
 
